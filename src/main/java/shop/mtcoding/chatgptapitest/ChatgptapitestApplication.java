d091122de988094720900f9d737690c98ca48835
unjoo <unjoo31@gmail.com>
2024-06-13 15:55:09 +0900

    first commit

---
+++ src/main/java/shop/mtcoding/chatgptapitest/ChatgptapitestApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.chatgptapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptapitestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgptapitestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/chatgptapitest/ChatgptapitestApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.chatgptapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptapitestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
